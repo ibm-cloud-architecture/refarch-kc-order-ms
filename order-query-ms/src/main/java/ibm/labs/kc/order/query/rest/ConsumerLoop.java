4e7ef3dbdb0f455c8e49bcf6cf9059be9b7533f6
Mickael Maison <mickael.maison@gmail.com>
2019-01-17 13:16:09 +0000

    Removed javaee-web-api, commented ConsumerLoop out

--- order-query-ms/src/main/java/ibm/labs/kc/order/query/rest/ConsumerLoop.java
+++ order-query-ms/src/main/java/ibm/labs/kc/order/query/rest/ConsumerLoop.java
@@ -6,7 +6,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Destroyed;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
-import javax.servlet.ServletContext;
 
 import ibm.labs.kc.order.query.dao.OrderDAO;
 import ibm.labs.kc.order.query.dao.OrderDAOMock;
@@ -16,35 +15,35 @@ import ibm.labs.kc.order.query.model.Order;
 @ApplicationScoped
 public class ConsumerLoop {
 
-    private OrderDAO orderDAO;
-    private OrderConsumer consumer;
-    private boolean running = true;
-
-    public void init(@Observes @Initialized(ApplicationScoped.class) ServletContext context) {
-        // Perform action during application's startup
-        orderDAO = OrderDAOMock.instance();
-
-        consumer = OrderConsumer.instance();
-        new Thread() {
-            public void run() {
-                while (running) {
-                    List<Order> orders = consumer.poll();
-                    for (Order o : orders) {
-                        orderDAO.upsert(o);
-                    }
-                }
-                consumer.close();
-            };
-        }.start();
-    }
-
-    public void destroy(@Observes @Destroyed(ApplicationScoped.class) ServletContext context) {
-        // Perform action during application's shutdown
-        running = false;
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+//    private OrderDAO orderDAO;
+//    private OrderConsumer consumer;
+//    private boolean running = true;
+//
+//    public void init(@Observes @Initialized(ApplicationScoped.class) ServletContext context) {
+//        // Perform action during application's startup
+//        orderDAO = OrderDAOMock.instance();
+//
+//        consumer = OrderConsumer.instance();
+//        new Thread() {
+//            public void run() {
+//                while (running) {
+//                    List<Order> orders = consumer.poll();
+//                    for (Order o : orders) {
+//                        orderDAO.upsert(o);
+//                    }
+//                }
+//                consumer.close();
+//            };
+//        }.start();
+//    }
+//
+//    public void destroy(@Observes @Destroyed(ApplicationScoped.class) ServletContext context) {
+//        // Perform action during application's shutdown
+//        running = false;
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
