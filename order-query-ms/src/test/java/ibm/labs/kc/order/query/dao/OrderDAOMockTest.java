a950efd8ec89f221c7da3b81bed9acf279b0d74a
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2019-03-05 20:44:34 -0600

    Adding OrderDAO Mock tests

--- order-query-ms/src/test/java/ibm/labs/kc/order/query/dao/OrderDAOMockTest.java
+++ order-query-ms/src/test/java/ibm/labs/kc/order/query/dao/OrderDAOMockTest.java
@@ -1,9 +1,13 @@
 package ibm.labs.kc.order.query.dao;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.Test;
@@ -12,6 +16,24 @@ import ibm.labs.kc.order.query.model.Address;
 import ibm.labs.kc.order.query.model.Order;
 
 public class OrderDAOMockTest {
+	
+	@Test
+    public void testGetById() {
+		OrderDAOMock dao = new OrderDAOMock();
+        Address addr = new Address("myStreet", "myCity", "myCountry", "myState", "myZipcode");
+        QueryOrder o1 = QueryOrder.newFromOrder(new Order("orderID", "productId", "custId1", 2,
+                addr, "2019-01-10T13:30Z",
+                addr, "2019-01-10T13:30Z", Order.PENDING_STATUS));
+        QueryOrder o2 = QueryOrder.newFromOrder(new Order(UUID.randomUUID().toString(), "productId", "custId2", 2,
+                addr, "2019-01-10T13:30Z",
+                addr, "2019-01-10T13:30Z", Order.PENDING_STATUS));
+
+        dao.add(o1);
+        dao.add(o2);
+
+        Optional<QueryOrder> byId = dao.getById("orderID");
+        assertEquals(o1, byId.orElse(null));
+    }
 
     @Test
     public void testGetByManuf() {
@@ -37,6 +59,55 @@ public class OrderDAOMockTest {
 
         Collection<QueryOrder> byManuf1 = dao.getByManuf("custId1");
         assertEquals(expected, new HashSet<QueryOrder>(byManuf1));
+    }
+    
+    @Test
+    public void testGetByStatus() {
+        OrderDAOMock dao = new OrderDAOMock();
+        Address addr = new Address("myStreet", "myCity", "myCountry", "myState", "myZipcode");
+        QueryOrder o1 = QueryOrder.newFromOrder(new Order(UUID.randomUUID().toString(), "productId", "custId1", 2,
+                addr, "2019-01-10T13:30Z",
+                addr, "2019-01-10T13:30Z", Order.PENDING_STATUS));
+        QueryOrder o2 = QueryOrder.newFromOrder(new Order(UUID.randomUUID().toString(), "productId", "custId2", 2,
+                addr, "2019-01-10T13:30Z",
+                addr, "2019-01-10T13:30Z", Order.ASSIGNED_STATUS));
+        QueryOrder o3 = QueryOrder.newFromOrder(new Order(UUID.randomUUID().toString(), "productId", "custId1", 2,
+                addr, "2019-01-10T13:30Z",
+                addr, "2019-01-10T13:30Z", Order.ASSIGNED_STATUS));
+
+        dao.add(o1);
+        dao.add(o2);
+        dao.add(o3);
+
+        Collection<QueryOrder> expected = new HashSet<>();
+        expected.add(o2);
+        expected.add(o3);
+
+        Collection<QueryOrder> byStatus = dao.getByStatus(Order.ASSIGNED_STATUS);
+        assertEquals(expected, new HashSet<QueryOrder>(byStatus));
+    }
+    
+    @Test
+    public void testGetOrderHistory() {
+        OrderDAOMock dao = new OrderDAOMock();
+        Address addr = new Address("myStreet", "myCity", "myCountry", "myState", "myZipcode");
+        QueryOrder o1 = QueryOrder.newFromOrder(new Order("orderID", "productId", "custId1", 2,
+                addr, "2019-01-10T13:30Z",
+                addr, "2019-01-10T13:30Z", Order.PENDING_STATUS));
+//        QueryOrder o2 = new QueryOrder("orderID","productId","custId1", 2, addr, "2019-01-10T13:30Z", addr, "2019-01-10T13:30Z", Order.ASSIGNED_STATUS, "myvoyageID");
+//        QueryOrder o3 = new QueryOrder(UUID.randomUUID().toString(),"productId","custId1", 2, addr, "2019-01-10T13:30Z", addr, "2019-01-10T13:30Z", Order.PENDING_STATUS);
+//        
+        dao.add(o1);
+        dao.orderHistory(o1);
+//        dao.orderHistory(o2);
+//        dao.orderHistory(o3);
+
+        Collection<QueryOrder> expected = new ArrayList<>();
+        expected.add(o1);
+//        expected.add(o2);
+
+        Collection<QueryOrder> byStatus = dao.getOrderStatus("orderID", "custId1");
+        assertArrayEquals(Collections.unmodifiableCollection(expected).toArray(),byStatus.toArray());
     }
 
 }
