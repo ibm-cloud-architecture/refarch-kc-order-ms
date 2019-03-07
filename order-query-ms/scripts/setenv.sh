035da905b7dfd284752a9537d67d70ac91927653
Jerome Boyer <boyerje@us.ibm.com>
2019-03-06 19:41:39 -0800

    Work on deploying iks

--- order-query-ms/scripts/setenv.sh
+++ order-query-ms/scripts/setenv.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-set p = $(echo $PWD | awk -v h="scripts" '$0 ~h')
-if [[ $PWD = */scripts ]]; then
- cd ..
-fi
 export msname="orderqueryms"
-export chart=$(ls ./chart/| grep $msname)
-export kname="kc-orderquery"
+export chart=$(ls ../chart/| grep $msname)
+export kname="kc-orderqueryms"
 export ns="browncompute"
