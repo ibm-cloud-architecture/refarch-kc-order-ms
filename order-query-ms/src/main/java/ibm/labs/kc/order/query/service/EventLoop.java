a8134e8252f11ae1370f3af93e88b870c746b20e
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2019-03-22 12:29:35 -0500

    Container consumer

--- order-query-ms/src/main/java/ibm/labs/kc/order/query/service/EventLoop.java
+++ order-query-ms/src/main/java/ibm/labs/kc/order/query/service/EventLoop.java
@@ -1,5 +1,6 @@
 package ibm.labs.kc.order.query.service;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -13,9 +14,9 @@ import org.slf4j.LoggerFactory;
 
 import ibm.labs.kc.order.query.action.OrderActionService;
 import ibm.labs.kc.order.query.kafka.ApplicationConfig;
+import ibm.labs.kc.order.query.kafka.ContainerConsumer;
 import ibm.labs.kc.order.query.kafka.ErrorProducer;
 import ibm.labs.kc.order.query.kafka.OrderConsumer;
-import ibm.labs.kc.order.query.model.Events;
 import ibm.labs.kc.order.query.model.events.ContainerEvent;
 import ibm.labs.kc.order.query.model.events.ErrorEvent;
 import ibm.labs.kc.order.query.model.events.EventEmitter;
@@ -24,34 +25,40 @@ import ibm.labs.kc.order.query.model.events.OrderEvent;
 
 @WebListener
 public class EventLoop implements ServletContextListener {
-    static final Logger logger = LoggerFactory.getLogger(EventLoop.class);
+	static final Logger logger = LoggerFactory.getLogger(EventLoop.class);
 
     private boolean running = true;
-    private OrderConsumer consumer;
-    private ExecutorService executor;
+    private OrderConsumer orderConsumer;
+    private ContainerConsumer containerConsumer;
+    private ExecutorService orderExecutor;
+    private ExecutorService containerExecutor;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         logger.info("ConsumerLoop contextInitialized");
 
-        consumer = new OrderConsumer();
-        executor = Executors.newFixedThreadPool(1);
-        executor.execute(newReloadRunnable());
-        executor.execute(newRunnable());
+        orderConsumer = new OrderConsumer();
+        orderExecutor = Executors.newFixedThreadPool(1);
+        orderExecutor.execute(newReloadOrderRunnable());
+        orderExecutor.execute(newOrderRunnable());
+        
+        containerConsumer = new ContainerConsumer();
+        containerExecutor = Executors.newFixedThreadPool(1);
+        containerExecutor.execute(newReloadContainerRunnable());
+        containerExecutor.execute(newContainerRunnable());
     }
 
-    private Runnable newReloadRunnable() {
+    private Runnable newReloadOrderRunnable() {
         return new Runnable() {
             @Override
             public void run() {
                 logger.info("ReloadState started");
                 EventListener queryServiceListener = new QueryService();
                 EventListener orderActionServicelistener = new OrderActionService();
-                EventListener containerServicelistener = new ContainerService();
 
-                while (!consumer.reloadCompleted()) {
-                    Events events = consumer.pollForReload();
-                    for (OrderEvent event : events.getOrderEvent()) {
+                while (!orderConsumer.reloadCompleted()) {
+                    List<OrderEvent> events = orderConsumer.pollForReload();
+                    for (OrderEvent event : events) {
                         try {
                         	queryServiceListener.handle(event);
                         	orderActionServicelistener.handle(event);
@@ -61,7 +68,25 @@ public class EventLoop implements ServletContextListener {
                             // mark the app as unhealthy
                         }
                     }
-                    for (ContainerEvent event : events.getContainerEvent()) {
+                }
+                
+                logger.info("ReloadState order completed");
+                orderConsumer.safeReloadClose();
+                
+            }
+        };
+    }
+    
+    private Runnable newReloadContainerRunnable() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                logger.info("ReloadState started");
+                EventListener containerServicelistener = new ContainerService();
+
+                while (!containerConsumer.reloadCompleted()) {
+                    List<ContainerEvent> events = containerConsumer.pollForReload();
+                    for (ContainerEvent event : events) {
                         try {
                         	containerServicelistener.handle(event);
                         } catch (Exception e) {
@@ -72,26 +97,26 @@ public class EventLoop implements ServletContextListener {
                     }
                 }
                 logger.info("ReloadState completed");
-                consumer.safeReloadClose();
+                containerConsumer.safeReloadClose();
             }
         };
     }
 
-    private Runnable newRunnable() {
+    private Runnable newOrderRunnable() {
         return new Runnable() {
             @Override
             public void run() {
                 logger.info("ConsumerLoop thread started");
                 EventListener queryServiceListener = new QueryService();
                 EventListener orderActionServicelistener = new OrderActionService();
-                EventListener containerServicelistener = new ContainerService();
+                
                 EventEmitter emitter = new ErrorProducer();
                 boolean ok = true;
                 try {
                     while (running && ok) {
                         try {
-                            Events events = consumer.poll();
-                            for (OrderEvent event : events.getOrderEvent()) {
+                            List<OrderEvent> ordEvents = orderConsumer.poll();
+                            for (OrderEvent event : ordEvents) {
                                 try {
                                 	queryServiceListener.handle(event);
                                 	orderActionServicelistener.handle(event);
@@ -106,7 +131,37 @@ public class EventLoop implements ServletContextListener {
                                     }
                                 }
                             }
-                            for (ContainerEvent event : events.getContainerEvent()) {
+                            
+                        } catch (Exception ke) {
+                            ke.printStackTrace();
+                            // Treat a Kafka exception as unrecoverable
+                            // stop this task and queue a new one
+                            ok = false;
+                            orderExecutor.execute(newOrderRunnable());
+                            // TODO: after a number of retrying, mark app as unhealthy for external monitoring restart
+                        }
+                    }
+                } finally {
+                	orderConsumer.safeClose();
+                    emitter.safeClose();
+                }
+            };
+        };
+    }
+    
+    private Runnable newContainerRunnable() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                logger.info("ConsumerLoop thread started");
+                EventListener containerServicelistener = new ContainerService();
+                EventEmitter emitter = new ErrorProducer();
+                boolean ok = true;
+                try {
+                    while (running && ok) {
+                        try {
+                            List<ContainerEvent> contEvents = containerConsumer.poll();
+                            for (ContainerEvent event : contEvents) {
                                 try {
                                 	containerServicelistener.handle(event);
                                 } catch (Exception e) {
@@ -125,12 +180,12 @@ public class EventLoop implements ServletContextListener {
                             // Treat a Kafka exception as unrecoverable
                             // stop this task and queue a new one
                             ok = false;
-                            executor.execute(newRunnable());
+                            containerExecutor.execute(newContainerRunnable());
                             // TODO: after a number of retrying, mark app as unhealthy for external monitoring restart
                         }
                     }
                 } finally {
-                    consumer.safeClose();
+                	containerConsumer.safeClose();
                     emitter.safeClose();
                 }
             };
@@ -141,12 +196,17 @@ public class EventLoop implements ServletContextListener {
     public void contextDestroyed(ServletContextEvent sce) {
         logger.info("ConsumerLoop contextDestroyed");
         running = false;
-        executor.shutdownNow();
+        orderExecutor.shutdownNow();
         try {
-            executor.awaitTermination(ApplicationConfig.TERMINATION_TIMEOUT_SEC, TimeUnit.SECONDS);
+        	orderExecutor.awaitTermination(ApplicationConfig.TERMINATION_TIMEOUT_SEC, TimeUnit.SECONDS);
+        } catch (InterruptedException ie) {
+            logger.warn("awaitTermination( interrupted", ie);
+        }
+        containerExecutor.shutdownNow();
+        try {
+        	containerExecutor.awaitTermination(ApplicationConfig.TERMINATION_TIMEOUT_SEC, TimeUnit.SECONDS);
         } catch (InterruptedException ie) {
             logger.warn("awaitTermination( interrupted", ie);
         }
     }
-
 }
